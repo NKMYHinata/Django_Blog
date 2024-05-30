887de5eb63348fcdcc343e8efd58563423179997
NKMYHinata <xiriy@outlook.com>
2024-05-30 15:49:27 +0800

    文章模型定义

--- home/models.py
+++ home/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils import timezone
+from users.models import User
+from django.utils import timezone
+
 
 # Create your models here.
 
@@ -20,3 +23,40 @@ class ArticleCategory(models.Model):
         db_table = 'tb_category'  # 修改表名
         verbose_name = '类别管理'  # admin站点显示
         verbose_name_plural = verbose_name  #
+
+
+class Article(models.Model):
+    # 1. 作者
+    # 参数on_delete就是当user表中的数据删除之后，文章信息也同步删除
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # 2. 标题图
+    avatar = models.ImageField(upload_to='article/%Y%m%d/', blank=True)
+    # 3. 标题
+    title = models.CharField(max_length=20, blank=True)
+    # 4. 栏目分类
+    category = models.ForeignKey(ArticleCategory, null=True, blank=True, on_delete=models.CASCADE,
+                                 related_name='article')
+    # 5. 标签
+    tags = models.CharField(max_length=20, blank=True)
+    # 6. 摘要信息
+    sumary = models.CharField(max_length=200, null=False, blank=False)
+    # 7. 文章正文
+    content = models.TextField()
+    # 8. 浏览量
+    total_views = models.PositiveSmallIntegerField(default=0)
+    # 9. 评论量
+    comments_count = models.PositiveSmallIntegerField(default=0)
+    # 10.创建时间
+    created = models.DateTimeField(default=timezone.now())
+    # 11.修改时间
+    update = models.DateTimeField(auto_now=True)
+
+    # 修改表名以及admin展示的配置信息等
+    class Mate:
+        db_table = 'tb_article'
+        ordering = ('-created',)
+        verbose_name = '文章管理'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
